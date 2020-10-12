dd7fe9cd3cc57ac030540d487c8440e6ce85bbd7
NashidForbes <NashidForbes@biohumans.com>
2020-10-11 21:00:42 -0400

    Updated security integration testing with Mockito.

--- src/test/java/guru/sfg/brewery/web/controllers/BeerControllerIT.java
+++ src/test/java/guru/sfg/brewery/web/controllers/BeerControllerIT.java
@@ -15,6 +15,7 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.httpBasic;
 import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
@@ -53,10 +54,22 @@ public class BeerControllerIT {
                 .build();
     }
 
-    @WithMockUser("spring")
+    // can be any user, creates an authenticated imaginary user
+    // not going through authentication manager
+    // just tests security logic
+    @WithMockUser("admin")
     @Test
     void findBeers() throws Exception{
         mockMvc.perform(get("/beers/find"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("beers/findBeers"))
+                .andExpect(model().attributeExists("beer"));
+    }
+
+    // tests authentication logic
+    @Test
+    void findBeersWithHttpBasic() throws Exception{
+        mockMvc.perform(get("/beers/find").with(httpBasic("admin", "admin")))
                 .andExpect(status().isOk())
                 .andExpect(view().name("beers/findBeers"))
                 .andExpect(model().attributeExists("beer"));
