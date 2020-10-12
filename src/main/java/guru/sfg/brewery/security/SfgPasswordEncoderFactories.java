8c37aa01e97fc8ecec3f8e1d4342ddea737eec90
NashidForbes <NashidForbes@biohumans.com>
2020-10-12 16:36:27 -0400

    Example using custom delegating password encoder.  Can select which password encoding to use for a user by e.g. bcrypt, sha256 and ldap encoding.
    Implemented custom Bcrypt password encoder of strength 15 for Scott's password.

--- src/main/java/guru/sfg/brewery/security/SfgPasswordEncoderFactories.java
+++ src/main/java/guru/sfg/brewery/security/SfgPasswordEncoderFactories.java
@@ -16,10 +16,21 @@ public class SfgPasswordEncoderFactories {
         String encodingId = "bcrypt";
         Map<String, PasswordEncoder> encoders = new HashMap<>();
         encoders.put(encodingId, new BCryptPasswordEncoder());
-        encoders.put("ldap", new org.springframework.security.crypto.password.LdapShaPasswordEncoder());
-        encoders.put("noop", org.springframework.security.crypto.password.NoOpPasswordEncoder.getInstance());
-        encoders.put("sha256", new org.springframework.security.crypto.password.StandardPasswordEncoder());
-
+        encoders.put("ldap",
+                new org.springframework.security.crypto.password.LdapShaPasswordEncoder());
+        encoders.put("noop",
+                org.springframework.security.crypto.password.NoOpPasswordEncoder
+                        .getInstance());
+        encoders.put("sha256",
+                new org.springframework.security.crypto.password.StandardPasswordEncoder());
+/*      You be the developer
+        Security and compliance has determined the strength of 10 is no longer sufficient
+        Recommendation is to use strength of 15 to prevent brute force attacks
+        Update Spring Security to use BCrypt with strength of 15
+        Use new key - bcrypt15
+        Update password for user scott only - others will be migrated at a future date*/
+        encoders.put("bcrypt15",
+                new BCryptPasswordEncoder(15));
         return new DelegatingPasswordEncoder(encodingId, encoders);
     }
 
