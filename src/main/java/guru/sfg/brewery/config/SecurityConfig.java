27b2017530d6bdeca884d6658fed1be59c2b57f7
NashidForbes <NashidForbes@biohumans.com>
2020-10-12 00:56:01 -0400

    Using 2 different In Memory Authentication methods from WebSecurityConfigurerAdapter ==> UserDetailsService and configure(AuthenticationManagerBuilder auth).

--- src/main/java/guru/sfg/brewery/config/SecurityConfig.java
+++ src/main/java/guru/sfg/brewery/config/SecurityConfig.java
@@ -3,9 +3,12 @@ package guru.sfg.brewery.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configurers.SecurityContextConfigurer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -35,7 +38,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .httpBasic();
     }
 
-    @Override
+/*    @Override
     @Bean
     protected UserDetailsService userDetailsService() {
         UserDetails admin = User.withDefaultPasswordEncoder()
@@ -51,20 +54,65 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .build();
 
         return new InMemoryUserDetailsManager(admin, user);
+    }*/
+
+
+    /**
+     * Used by the default implementation of {@link #authenticationManager()} to attempt
+     * to obtain an {@link AuthenticationManager}. If overridden, the
+     * {@link AuthenticationManagerBuilder} should be used to specify the
+     * {@link AuthenticationManager}.
+     *
+     * <p>
+     * The {@link #authenticationManagerBean()} method can be used to expose the resulting
+     * {@link AuthenticationManager} as a Bean. The {@link #userDetailsServiceBean()} can
+     * be used to expose the last populated {@link UserDetailsService} that is created
+     * with the {@link AuthenticationManagerBuilder} as a Bean. The
+     * {@link UserDetailsService} will also automatically be populated on
+     * {@link HttpSecurity#getSharedObject(Class)} for use with other
+     * {@link SecurityContextConfigurer} (i.e. RememberMeConfigurer )
+     * </p>
+     *
+     * <p>
+     * For example, the following configuration could be used to register in memory
+     * authentication that exposes an in memory {@link UserDetailsService}:
+     * </p>
+     *
+     * <pre>
+     * &#064;Override
+     * protected void configure(AuthenticationManagerBuilder auth) {
+     * 	auth
+     * 	// enable in memory based authentication with a user named
+     * 	// &quot;user&quot; and &quot;admin&quot;
+     * 	.inMemoryAuthentication().withUser(&quot;user&quot;).password(&quot;password&quot;).roles(&quot;USER&quot;).and()
+     * 			.withUser(&quot;admin&quot;).password(&quot;password&quot;).roles(&quot;USER&quot;, &quot;ADMIN&quot;);
+     * }
+     *
+     * // Expose the UserDetailsService as a Bean
+     * &#064;Bean
+     * &#064;Override
+     * public UserDetailsService userDetailsServiceBean() throws Exception {
+     * 	return super.userDetailsServiceBean();
+     * }
+     *
+     * </pre>
+     *
+     * @param auth the {@link AuthenticationManagerBuilder} to use
+     * @throws Exception
+     */
+    @Override // a fluent api
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("spring")
+                .password("{noop}guru") // noop is to skip password encoding / encryption
+                .roles("ADMIN")
+                .and()
+                .withUser("user")
+                .password("{noop}password") // noop is to skip password encoding / encryption
+                .roles("USER")
+                .and()
+                .withUser("user2")
+                .password("{noop}password") // noop is to skip password encoding / encryption
+                .roles("USER");
     }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 }
