dd7939679efc2909e468087663126e22176676cd
NashidForbes <NashidForbes@biohumans.com>
2020-10-14 02:58:45 -0400

    URL parameter authentication filter challenge, implement your own custom URL Parameter Auth filter.

--- src/main/java/guru/sfg/brewery/security/RestUrlAuthFilter.java
+++ src/main/java/guru/sfg/brewery/security/RestUrlAuthFilter.java
@@ -1,9 +1,16 @@
 package guru.sfg.brewery.security;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.util.matcher.RequestMatcher;
+import org.springframework.util.StringUtils;
 
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 // DRY principle Do Not Repeat Yourself
 @Slf4j
@@ -20,6 +27,33 @@ public class RestUrlAuthFilter extends
             RequestMatcher requiresAuthenticationRequestMatcher) {
         super(requiresAuthenticationRequestMatcher);
     }
+
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws
+            AuthenticationException, IOException, ServletException {
+        String userName = getUsername(request);
+        String password = getPassword(request);
+
+        if (userName == null){
+            userName = "";
+        }
+
+        if (password == null){
+            password = "";
+        }
+
+        log.debug("Authenticating User: " + userName);
+
+        UsernamePasswordAuthenticationToken
+                token = new UsernamePasswordAuthenticationToken(userName, password);
+
+        if (!StringUtils.isEmpty(userName)) {
+            return this.getAuthenticationManager().authenticate(token);
+        } else {
+            return null;
+        }
+    }
+
 
     protected String getPassword(HttpServletRequest request) {
         return request.getParameter("apiSecret");
