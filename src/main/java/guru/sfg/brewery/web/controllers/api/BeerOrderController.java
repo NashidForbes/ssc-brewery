5a43c76a050e4894c06bcc004c8c085eedf12815
NashidForbes <NashidForbes@biohumans.com>
2020-11-03 02:26:53 -0500

    Working security annotation for Beer Order create, read and pickup.  But pickup annotation is not functional.

--- src/main/java/guru/sfg/brewery/web/controllers/api/BeerOrderController.java
+++ src/main/java/guru/sfg/brewery/web/controllers/api/BeerOrderController.java
@@ -17,12 +17,14 @@
 
 package guru.sfg.brewery.web.controllers.api;
 
+import guru.sfg.brewery.security.perms.BeerOrderCreatePermission;
+import guru.sfg.brewery.security.perms.BeerOrderPickupPermission;
+import guru.sfg.brewery.security.perms.BeerOrderReadPermission;
 import guru.sfg.brewery.services.BeerOrderService;
 import guru.sfg.brewery.web.model.BeerOrderDto;
 import guru.sfg.brewery.web.model.BeerOrderPagedList;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.http.HttpStatus;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
@@ -43,42 +45,43 @@ public class BeerOrderController {
         this.beerOrderService = beerOrderService;
     }
 
-    @PreAuthorize("hasAuthority('order.read') OR " +
-            "hasAuthority('customer.order.read') " +
-            " AND @beerOrderAuthenticationManger.customerIdMatches(authentication, #customerId )")
+    @BeerOrderReadPermission
     @GetMapping("orders")
     public BeerOrderPagedList listOrders(@PathVariable("customerId") UUID customerId,
-                                         @RequestParam(value = "pageNumber", required = false) Integer pageNumber,
-                                         @RequestParam(value = "pageSize", required = false) Integer pageSize){
-
-        if (pageNumber == null || pageNumber < 0){
+                                         @RequestParam(value = "pageNumber", required =
+                                                 false) Integer pageNumber,
+                                         @RequestParam(value = "pageSize", required =
+                                                 false) Integer pageSize) {
+        if (pageNumber == null || pageNumber < 0) {
             pageNumber = DEFAULT_PAGE_NUMBER;
         }
-
         if (pageSize == null || pageSize < 1) {
             pageSize = DEFAULT_PAGE_SIZE;
         }
-
-        return beerOrderService.listOrders(customerId, PageRequest.of(pageNumber, pageSize));
+        return beerOrderService
+                .listOrders(customerId, PageRequest.of(pageNumber, pageSize));
     }
 
+    @BeerOrderCreatePermission
     @PostMapping("orders")
     @ResponseStatus(HttpStatus.CREATED)
-    public BeerOrderDto placeOrder(@PathVariable("customerId") UUID customerId, @RequestBody BeerOrderDto beerOrderDto){
+    public BeerOrderDto placeOrder(@PathVariable("customerId") UUID customerId,
+                                   @RequestBody BeerOrderDto beerOrderDto) {
         return beerOrderService.placeOrder(customerId, beerOrderDto);
     }
 
-    @PreAuthorize("hasAuthority('order.read') OR " +
-            "hasAuthority('customer.order.read') " +
-            " AND @beerOrderAuthenticationManger.customerIdMatches(authentication, #customerId )")
+    @BeerOrderCreatePermission
     @GetMapping("orders/{orderId}")
-    public BeerOrderDto getOrder(@PathVariable("customerId") UUID customerId, @PathVariable("orderId") UUID orderId){
+    public BeerOrderDto getOrder(@PathVariable("customerId") UUID customerId,
+                                 @PathVariable("orderId") UUID orderId) {
         return beerOrderService.getOrderById(customerId, orderId);
     }
 
+    @BeerOrderPickupPermission
     @PutMapping("/orders/{orderId}/pickup")
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void pickupOrder(@PathVariable("customerId") UUID customerId, @PathVariable("orderId") UUID orderId){
+    public void pickupOrder(@PathVariable("customerId") UUID customerId,
+                            @PathVariable("orderId") UUID orderId) {
         beerOrderService.pickupOrder(customerId, orderId);
     }
 }
